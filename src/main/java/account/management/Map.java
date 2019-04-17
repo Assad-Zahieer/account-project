20316ca20c63e25407d8d16e826619e97ae440c5
Assad Zahieer <aa.zahieer@gmail.com>
2019-04-17 07:09:04 +0100

    printing as JSON

--- src/main/java/account/management/Map.java
+++ src/main/java/account/management/Map.java
@@ -3,7 +3,10 @@ package account.management;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
+import com.google.gson.Gson;
+
 public class Map {
+	static Gson gson = new Gson();
 	static HashMap<Integer, Account>hash = new HashMap<Integer, Account>();
 	static int key = 1;
 	public static void addAccount(String firstName, String lastName, String accountNumber) {
@@ -13,14 +16,18 @@ public class Map {
 	}
 	public static void retrieveAccount() {
 		for (Account account: hash.values()) {
-			System.out.println(account);
+//			System.out.println(account); Prints ram location
+			String json = gson.toJson(account);
+			System.out.println(json);
+			
 		}		
 	}
 	public static void retrieveEntries() {
 		for(Entry<Integer, Account> entry : hash.entrySet()) {
 			int keyValue = entry.getKey();
 			Account account = entry.getValue();
-			System.out.println(keyValue + " : " + account);
+			String json = gson.toJson(account);
+			System.out.println(keyValue +" : " + json);
 		}
 		
 	}
